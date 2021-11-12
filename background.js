ccc1c6bf15a36a75849f60b266c3675ba9649b71
Ryan Detzel <rdetzel@twitter.com>
2021-11-12 09:44:29 -0500

    remove the onInstalled event

--- background.js
+++ background.js
@@ -3,38 +3,37 @@
 const Temperature = 174;
 const Brightness = 25;
 
-chrome.runtime.onInstalled.addListener(() => {
-  // console.log("Starting up");
+chrome.tabs.onRemoved.addListener((tab) => {
+  checkTabs();
+});
 
-  chrome.tabs.onRemoved.addListener((tab) => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo && changeInfo.status === "complete"){
     checkTabs();
-  });
-
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo && changeInfo.status === "complete"){
-      checkTabs();
-    }
-  });
-
-  const checkTabs = () => {
-    chrome.tabs.query({"url":"*://meet.google.com/*"}, (tabs) => {
-      if (tabs.length > 0){
-        // console.log("Meet tab is open");
-        putRequest({"Lights":[{"Temperature":Temperature,"Brightness":Brightness,"On":1}]});
-      } else {
-        putRequest({"Lights":[{"Temperature":Temperature,"Brightness":Brightness,"On":0}]});
-      }
-    });
-  }
-
-  const putRequest = (payload) => {
-    const url = "http://192.168.1.150:9123/elgato/lights";
-
-    fetch(url, {
-      method: "PUT",
-      body: JSON.stringify(payload),
-    })
-    .then(response => response)
-    .catch(error => {console.log(error)})
   }
 });
+
+const checkTabs = () => {
+  chrome.tabs.query({"url":"*://meet.google.com/*"}, (tabs) => {
+    if (tabs.length > 0){
+      putRequest({"Lights":[{"Temperature":Temperature,"Brightness":Brightness,"On":1}]});
+    } else {
+      putRequest({"Lights":[{"Temperature":Temperature,"Brightness":Brightness,"On":0}]});
+    }
+  });
+}
+
+const putRequest = (payload) => {
+  const url = "http://192.168.1.150:9123/elgato/lights";
+
+  fetch(url, {
+    method: "PUT",
+    body: JSON.stringify(payload),
+  })
+  .then(response => {
+    console.log("done");
+  })
+  .catch(error => {
+    console.log(error);
+  });
+}
